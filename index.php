468e86dbb1a7ca5405858f7eb872e03433d505f7
ertimon <ertik1997@mail.ru>
2019-08-15 23:02:50 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+function binarySearch($file, $searchingKey){
+    $handle = fopen($file, "r");
+
+    while(!feof($handle)){
+        $data = fgets($handle, 4000);
+        $split = explode('\x0A', $data);
+
+        foreach($split as $key => $value){
+            $arr[] = explode('\t', $value);
+        }
+
+        $start = 0;
+        $end = count($arr) - 1;
+
+        while ($start <= $end) {
+            $half = floor(($start + $end) / 2);
+            $check = strnatcmp($arr[$half][0], $searchingKey);
+            if ($check == 0){
+                return $arr[$half][1];
+            } elseif ($check < 0){
+                $start = $half + 1;
+            } elseif ($check > 0){
+                $end = $half - 1;
+            }
+        }
+    }
+    return 'undef';
+}
+?>
